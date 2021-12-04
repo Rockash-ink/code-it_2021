2c535ea7325d30d5af5b7dc698c285d999bebec9
ignzio <62619799+ignzio@users.noreply.github.com>
2021-12-04 02:54:38 +0000

    fixed indentation

--- Code-IT/src/App.java
+++ Code-IT/src/App.java
@@ -1,11 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("scemo");
+        System.out.println("potaa");
         System.out.println("hi im myleeeee");
-        System.out.println("You like poop");
+        System.out.println("brandon");
         System.out.println("take ignazio from here pls");
-        System.out.println("MUIE PSD");
-
 
     }
 }
