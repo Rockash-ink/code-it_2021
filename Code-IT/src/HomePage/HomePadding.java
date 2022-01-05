425bd1636e865ce1a323988ed5c083958a6593b2
Tony Ciubotaru <rockashink@gamil.com>
2022-01-05 21:59:18 +0000

    homepadding buttons

--- Code-IT/src/HomePage/HomePadding.java
+++ Code-IT/src/HomePage/HomePadding.java
@@ -1,30 +1,105 @@
 package HomePage;
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.BorderLayout;
-
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 
-public class HomePadding extends JLabel{
+
+public class HomePadding extends JPanel{
     HomeFeed homeFeed_Jlabel = new HomeFeed();
     JLabel a = new JLabel();
     JLabel b = new JLabel();
+    private String Text1 = "Use";
+    private String Text2 = "Preview";
+    JPanel mainPanel_homePadding;
+  
+  
     HomePadding(){
-        paddingSettings();
-        this.add(homeFeed_Jlabel,BorderLayout.CENTER);
+    paddingSettings();
 
-        this.a.setPreferredSize(new Dimension(100,50));
+        
+    this.a.setPreferredSize(new Dimension(100,50));
+   a.setOpaque(true);
+   a.setBackground(Color.YELLOW);
+   b.setLayout(new BorderLayout());
+    this.b.setPreferredSize(new Dimension(100,50));
+    b.setOpaque(true);
+    b.setBackground(Color.yellow);
+    JScrollBar hbar = new JScrollBar(JScrollBar.HORIZONTAL, 30, 20, 0, 500);
+    JScrollBar vbar = new JScrollBar(JScrollBar.VERTICAL, 30, 40, 0, 500);
 
-        this.add(a,BorderLayout.WEST);
-        this.add(b,BorderLayout.EAST);
 
+    b.add(hbar);
+    b.add(vbar);
+
+
+    this.add(a,BorderLayout.WEST);
+    this.add(b,BorderLayout.EAST);
+
+       
     }
+
+
     void paddingSettings(){
-        this.setPreferredSize(new Dimension(250,250));
+        homeFeed_Jlabel.setLayout(new BorderLayout());     
+        JScrollPane scrollFrame = new JScrollPane(homeFeed_Jlabel);
+        this.setAutoscrolls(true);
+        scrollFrame.setPreferredSize(new Dimension(800,300));
+        this.add(scrollFrame);
         this.setOpaque(true);
         this.setBackground(Color.BLACK);
-        this.setLayout(new BorderLayout(10,10));
-    }
+        this.setLayout(new BorderLayout(0,0));
+    
+        
+
+// Main panel 
+
+         mainPanel_homePadding = new JPanel( new FlowLayout(FlowLayout.CENTER, 10, 10) );
+         mainPanel_homePadding.setBackground(Color.GRAY);
+
+            
+        // Panel 1
+
+                JPanel panel = new JPanel();
+                panel.setPreferredSize(new Dimension(150,150));
+                panel.setBackground(Color.lightGray);
+                panel.setLayout(new FlowLayout());
+                panel.add(new JButton(Text1));
+                panel.add(new JButton(Text2));
+                mainPanel_homePadding.add(panel);
+        
+        // Panel 2
+
+                JPanel panel2 = new JPanel();
+                panel2.setPreferredSize(new Dimension(150,150));
+                panel2.setBackground(Color.lightGray);
+                panel2.setLayout(new FlowLayout());
+                panel2.add(new JButton("Use"));
+                panel2.add(new JButton("Preview"));
+                mainPanel_homePadding.add(panel2);
+
+        // Panel 3
+
+                JPanel panel3 = new JPanel();
+                panel3.setPreferredSize(new Dimension(150,150));
+                panel3.setBackground(Color.lightGray);
+                panel3.setLayout(new FlowLayout());
+                panel3.add(new JButton("Use"));
+                panel3.add(new JButton("Preview"));
+                panel.setVerifyInputWhenFocusTarget(true);
+                mainPanel_homePadding.add(panel3);
+
+        this.add(mainPanel_homePadding);
+        
+        
+}
+ 
+   
+    
     
 }
--- Code-IT/src/HomePage/TopSideHome.java
+++ Code-IT/src/HomePage/TopSideHome.java
@@ -3,6 +3,8 @@ package HomePage;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Color;
@@ -11,34 +13,43 @@ import java.awt.Color;
 class TopSideHome extends JPanel {
     JLabel welcomeText_topSideHome_JLabel = new JLabel(); //label with
     JButton rapidResearchButton_JButton = new JButton();
+    JButton topButtonE_JLabel = new JButton("Quick Search");
+    JTextArea topTextArea_JLabel = new JTextArea(" Hello  \n Welcome back!");
     JLabel topSide_JLabel = new JLabel();
     
-// muidauofbeav w ew siweo  :))
+// left side top button 
     TopSideHome(){
         this.setBackground(Color.GREEN);
-        this.setPreferredSize(new Dimension(100,100));
+        this.setPreferredSize(new Dimension(50,50));
         this.setLayout(new BorderLayout(5,5));
         welcomeTextSettings();
         rapidResearchButtonSetiings();
         topSideSettings();
     }
+    // Left hadside top button
     void welcomeTextSettings(){
         welcomeText_topSideHome_JLabel.setPreferredSize(new Dimension(250,250));
         welcomeText_topSideHome_JLabel.setOpaque(true);
-        welcomeText_topSideHome_JLabel.setBackground(Color.yellow); //
-        this.add(welcomeText_topSideHome_JLabel,BorderLayout.WEST);
+        welcomeText_topSideHome_JLabel.setBackground(Color.yellow); 
+        this.add(topTextArea_JLabel,BorderLayout.WEST);
     }
+
+    // Right handside top button
     void rapidResearchButtonSetiings(){
-        rapidResearchButton_JButton.setPreferredSize(new Dimension(100,100));
+        rapidResearchButton_JButton.setPreferredSize(new Dimension(150,50));
         this.add(rapidResearchButton_JButton,BorderLayout.EAST);
+        this.add(topButtonE_JLabel,BorderLayout.EAST);
+        
     }
+    
     void topSideSettings(){
-        topSide_JLabel.setPreferredSize(new Dimension(250,250));
-        topSide_JLabel.setText("<html>Welcome back!<br/>Im Happy to see you!</html>");
+        topSide_JLabel.setPreferredSize(new Dimension(150,250));
+        topSide_JLabel.setText("<html>INSERT IMAGE HERE </html>");
         topSide_JLabel.setOpaque(true);
         topSide_JLabel.setBackground(Color.orange);
         this.add(topSide_JLabel);
     }
+  
 
 
 }
